d9307b15d597c7046d7eefb0e8ce999b7f579f9c
Ben Groves <benjamin.groves@letgo.com>
2019-10-26 13:08:29 +0200

    using the stopwatch timer module for timer functionality

--- App.js
+++ App.js
@@ -1,170 +1,162 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableHighlight, ListView } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet,Text,View, TouchableHighlight, TextInput } from 'react-native';
+import { Timer } from 'react-native-stopwatch-timer';
 
-let laps = [
-  { name: 'Lap 1', value: '00.00.01'},
-  { name: 'Lap 2', value: '00.00.02'},
-  { name: 'Lap 3', value: '00.00.03'},
-  { name: 'Lap 4', value: '00.00.04'},
-  { name: 'Lap 5', value: '00.00.05'},
-];
-
-let ds = new ListView.DataSource({
-  rowHasChanged: (row1, row2) => row1 !== row2,
-});
-
-class Stopwatch extends React.Component {
+export default class HfitTimer extends Component {
   constructor(props) {
     super(props);
-    
     this.state = {
-       dataSource: ds.cloneWithRows(laps)
-     }
-   }
- }
+      isTimerStart: false,
+      timerDuration: 6000,
+      resetTimer: false,
+    };
+    this.startStopTimer = this.startStopTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+  }
+  startStopTimer() {
+    this.setState({isTimerStart: !this.state.isTimerStart, resetTimer: false});
+  }
+  resetTimer() {
+    this.setState({isTimerStart: false, resetTimer: true});
+  }
+  getFormattedTime(time) {
+      this.currentTime = time;
+  }
 
-export default class App extends React.Component {
+  _renderTimeInput() {
+    return(
+      <View>
+        <Text>Time:</Text>
+        <TextInput
+          keyboardType={"numeric"}
+          value={this.state.timerDuration}
+          onChangeText={(timerDuration) => this.setState({timerDuration})} />
+      </View>
+    )
+  }
+
   _renderTitle() {
     return (
       <View style={styles.header}>
-        <Text style={styles.title}>Stopwatch</Text>
-      </View>  
+        <Text style={styles.title}>HFIT</Text>
+      </View>
     )
   }
-  
-  _renderTimers() {
+
+  _renderTimer() {
     return (
       <View style={styles.timerWrapper}>
         <View style={styles.timerWrapperInner}>
           <Text style={styles.lapTimer}>00:00.95</Text>
-          <Text style={styles.mainTimer}>00:02.95</Text>
+          <Timer
+            totalDuration={this.state.timerDuration} secs
+            //Time Duration
+            start={this.state.isTimerStart}
+            //To start
+            reset={this.state.resetTimer}
+            //To reset
+            options={options}
+            //options for the styling
+            handleFinish={handleTimerComplete}
+            //can call a function On finish of the time
+            getTime={this.getFormattedTime} />
         </View>
       </View>
     )
   }
-  
+
   _renderButtons() {
     return (
       <View style={styles.buttonWrapper}>
-        <TouchableHighlight underlayColor='#777' style={styles.button}>
-          <Text>Lap</Text>
+        <TouchableHighlight onPress={this.startStopTimer} underlayColor='#777' style={styles.button}>
+          <Text>
+            {!this.state.isTimerStart ? "START" : "STOP"}
+          </Text>
         </TouchableHighlight>
-        <TouchableHighlight underlayColor='#777' style={styles.button}>
-          <Text>Start</Text>
+        <TouchableHighlight onPress={this.resetTimer} underlayColor='#777' style={styles.button}>
+          <Text>RESET</Text>
         </TouchableHighlight>
       </View>
     )
   }
-  
-  _renderLaps() {
-    return (
-      <View style={styles.lapsWrapper}>
-        <ListView
-          enableEmptySections={true}
-          dataSource={this.state.dataSource}
-          renderRow={ (rowData) => (
-            <View style={styles.lapRow}>
-              <Text style={style.lapNumber}>{rowData.name}</Text>
-              <Text style={style.lapTime}>{rowData.value}</Text>
-            </View>
-          )}
-        />
-      </View>
-    )
-  }
-  
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.top}>
-          {this._renderTitle()}
-          {this._renderTimers()}
+            {this._renderTitle()}
+            {this._renderTimer()}
         </View>
         <View style={styles.bottom}>
-          {this._renderButtons()}
+            {this._renderButtons()}
         </View>
       </View>
-    )
+    );
   }
 }
 
-// let laps = [
-//   { name: 'Lap 1', value: '00.00.01'},
-//   { name: 'Lap 2', value: '00.00.02'},
-//   { name: 'Lap 3', value: '00.00.03'},
-//   { name: 'Lap 4', value: '00.00.04'},
-//   { name: 'Lap 5', value: '00.00.05'},
-// ];
-
-// let ds = new ListView.DataSource({
-//   rowHasChanged: (row1, row2) => row1 !== row2,
-// });
-
-// class Stopwatch extends React.Component {
-//   constructor(props) {
-//     super(props);
-    
-//     this.state = {
-//       dataSource: ds.cloneWithRows(laps)
-//     }
-//   }
-// }
+const handleTimerComplete = () => alert("Custom Completion Function");
+const options = {
+  text: {
+    fontSize: 60,
+    fontWeight: '100',
+    alignSelf: 'flex-end',
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  
+
   header: {
-    borderBottomWidth: 0.5,
-    paddingTop: 20,
+    paddingTop: 30,
     paddingBottom: 10,
     backgroundColor: '#F9F9F9'
   },
-  
+
   title: {
     alignSelf: 'center',
     fontWeight: '600',
   },
-  
+
   top: {
     flex: 1,
   },
-  
+
   bottom: {
     flex: 2,
     backgroundColor: '#F0EFF5'
   },
-  
+
   timerWrapper: {
     backgroundColor: '#FFFFFF',
     justifyContent: 'center',
     flex: 100,
   },
-  
+
   timerWrapperInner: {
-    borderWidth: 0.5,
     alignSelf: 'center',
   },
-  
+
   mainTimer: {
     fontSize: 60,
     fontWeight: '100',
     alignSelf: 'flex-end'
   },
-  
+
   lapTimer: {
     fontSize: 18,
     alignSelf: 'flex-end'
   },
-  
+
   buttonWrapper: {
   flexDirection: 'row',
     justifyContent: 'space-around',
     paddingTop: 15,
     paddingBottom: 30,
 },
-  
+
   button: {
     height: 80,
     width: 80,
@@ -173,7 +165,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  
+
   lapRow: {
    flexDirection: 'row',
    justifyContent: 'space-around',
@@ -182,12 +174,12 @@ const styles = StyleSheet.create({
    borderBottomWidth: 0.5,
    borderBottomColor: '#ddd'
   },
-  
+
   lapNumber: {
     fontSize: 16,
     color: '#777'
   },
-  
+
   lapTime: {
     color: '#000',
     fontSize: 20,
